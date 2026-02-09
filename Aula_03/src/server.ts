cb98f70d01c823fead636e1d83869cee9755eefa
BrunaElohaBonk <brunaelohabonk698@gmail.com>
2026-02-09 10:28:07 -0300

    Atualização

--- Aula_03/src/server.ts
+++ Aula_03/src/server.ts
@@ -3,28 +3,6 @@ import express, { response } from 'express'
 const port = 8080
 const app = express()
 
-const pessoas = [
-    {
-        nome: "Vitoria"
-    },
-    {
-        nome: "Thales"
-    }
-]
-
-const pessoa = {
-    name: "Bruna",
-    lastname: "Bonk"
-}
-
-app.get('/objeto', (req, res) => {
-    res.send({ pessoa: pessoa })
-})
-
-app.get('/direto', (req, res) => {
-    res.send({ pessoas })
-})
-
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
 })
